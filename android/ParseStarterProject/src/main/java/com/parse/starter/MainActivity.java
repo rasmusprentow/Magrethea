e601c9807ae45b2409c60d44b62b2ddea5a43968
Rasmus Prentow <rasmus@prentow.dk>
2015-10-20 22:59:06 +0200

    Added initial stuff

---
+++ android/ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright (c) 2015-present, Parse, LLC.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+package com.parse.starter;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.transition.Visibility;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.parse.ParseAnalytics;
+import com.parse.ParseObject;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import bolts.Continuation;
+import bolts.Task;
+
+
+public class MainActivity extends Activity {
+
+
+  private Button mUnlockButton;
+    private EditText mCodeText;
+    private EditText mMasterKeyText;
+    private TextView mMasterKeyLabel;
+
+  private ProgressDialog progDailog;
+
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+    setContentView(R.layout.activity_main);
+
+
+    mUnlockButton = (Button) findViewById(R.id.unlock_button);
+    mCodeText = (EditText) findViewById(R.id.codeEditText);
+    mMasterKeyText = (EditText) findViewById(R.id.masterKeyEditText);
+      mMasterKeyLabel = (TextView) findViewById(R.id.masterKeyTextView);
+
+      hideIfMasterKeySet();
+
+
+      mUnlockButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+
+        //setProgressBarIndeterminateVisibility(true);
+        if( ! UnlockManager.getInstance().hasMasterKey(MainActivity.this)) {
+            UnlockManager.getInstance().setMasterKey(MainActivity.this, mMasterKeyText.getText().toString());
+            hideIfMasterKeySet();
+        }
+
+
+        progDailog = ProgressDialog.show(MainActivity.this, "Saving", "Wait for it", true);
+        UnlockManager.getInstance().deleteAll().continueWith(new Continuation<Object, Void>() {
+          @Override
+          public Void then(Task<Object> task) throws Exception {
+
+            UnlockManager.getInstance().setNewCode(MainActivity.this, "").continueWith(new Continuation<Void, Object>() {
+
+                @Override
+                public Object then(Task<Void> task) throws Exception {
+                    //setProgressBarIndeterminateVisibility(false);
+                    progDailog.dismiss();
+                    return null;
+                }
+            });
+            return null;
+          }
+        });
+
+
+
+      }
+    });
+
+
+    ParseAnalytics.trackAppOpenedInBackground(getIntent());
+  }
+
+    private void hideIfMasterKeySet() {
+        if(UnlockManager.getInstance().hasMasterKey(this)){
+            mMasterKeyLabel.setVisibility(View.GONE);
+            mMasterKeyText.setVisibility(View.GONE);
+        }
+    }
+
+
+
+    @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.menu_main, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+
+    //noinspection SimplifiableIfStatement
+    if (id == R.id.action_settings) {
+      return true;
+    }
+
+    return super.onOptionsItemSelected(item);
+  }
+}
---
+++ android/ParseStarterProject/src/main/java/com/parse/starter/UnlockManager.java
@@ -0,0 +1,121 @@
+package com.parse.starter;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.widget.Toast;
+
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+
+import bolts.Continuation;
+import bolts.Task;
+
+/**
+ * Created by pseudo on 20/10/15.
+ */
+public class UnlockManager {
+
+
+    public static String tableName = "Unlock";
+    public static String masterKeyName = "MASTERKEY";
+
+    private UnlockManager() {}
+
+    private static UnlockManager instance;
+    public static UnlockManager getInstance()
+    {
+        if(instance == null) {
+         instance = new UnlockManager();
+     }
+        return instance;
+    }
+    public Task<Object> deleteAll()
+    {
+        ParseQuery<ParseObject> query = ParseQuery.getQuery(tableName);
+
+        return  query.findInBackground().continueWith(new Continuation<List<ParseObject>, Object>() {
+            @Override
+            public Object then(Task<List<ParseObject>> task) throws Exception {
+                return ParseObject.deleteAllInBackground(task.getResult());
+
+            }
+        });
+    }
+
+    public Task<Void> setNewCode(Activity activity, String code){
+
+        String hashedValue = sha1Hash(code + ":" + getMasterKey(activity));
+
+        ParseObject gameScore = new ParseObject(tableName);
+        gameScore.put("passkey", hashedValue);
+        gameScore.put("unlock", true);
+        return gameScore.saveInBackground();
+    }
+
+    public boolean hasMasterKey(Activity activity) {
+        String key = getMasterKey(activity);
+        Toast.makeText(activity, "Masterkey is: " + key, Toast.LENGTH_SHORT ).show();
+        return (key != null && key.length() > 0);
+    }
+
+    private String getMasterKey(Activity activity){
+        return activity.getSharedPreferences(this.getClass().getName(), 0).getString(masterKeyName, "");
+
+    }
+
+    public void setMasterKey(Activity activity, String masterCode){
+        SharedPreferences settings = activity.getSharedPreferences(this.getClass().getName(), 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString(masterKeyName, masterCode);
+
+        // Commit the edits!
+        editor.commit();
+
+    }
+
+
+    String sha1Hash( String toHash )
+    {
+        String hash = null;
+        try
+        {
+            MessageDigest digest = MessageDigest.getInstance( "SHA-1" );
+            byte[] bytes = toHash.getBytes("UTF-8");
+            digest.update(bytes, 0, bytes.length);
+            bytes = digest.digest();
+
+            // This is ~55x faster than looping and String.formating()
+            hash = bytesToHex( bytes );
+        }
+        catch( NoSuchAlgorithmException e )
+        {
+            e.printStackTrace();
+        }
+        catch( UnsupportedEncodingException e )
+        {
+            e.printStackTrace();
+        }
+        return hash;
+    }
+
+    // http://stackoverflow.com/questions/9655181/convert-from-byte-array-to-hex-string-in-java
+    final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
+    public static String bytesToHex( byte[] bytes )
+    {
+        char[] hexChars = new char[ bytes.length * 2 ];
+        for( int j = 0; j < bytes.length; j++ )
+        {
+            int v = bytes[ j ] & 0xFF;
+            hexChars[ j * 2 ] = hexArray[ v >>> 4 ];
+            hexChars[ j * 2 + 1 ] = hexArray[ v & 0x0F ];
+        }
+        return new String( hexChars );
+    }
+
+
+}
